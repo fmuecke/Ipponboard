8acbc8176c3379d7b22f8f563c3c7a40489c1e55
fmuecke <fmuecke@2deb2235-3218-4295-bc31-02dd675da4f7>
2011-02-27 20:22:31 +0000

    ScaledText Widget: - now supports three different sizes/types:   --> normal: full ascent and descent   --> uppercase: text is made uppercase and with no descent (ascent is minimized)   --> full: minimum text size for upper case letters and numbers ==> max. scaling - some internal optimizations

--- widgets/scaledtext.cpp
+++ widgets/scaledtext.cpp
@@ -11,45 +11,32 @@ ScaledText::ScaledText( QWidget* pParent )
 	, m_pLayout(0)
 	, m_timerId(-1)
 	, m_timerDelay(0)
-	, m_isDigitSize(false)
+	, m_textSize(eSize_normal)
 	, m_isVisible(true)
 	, m_rotate(false)
 {
 	SetText( "ScaledText" );
 }
 
-//ScaledText::ScaledText( const QFont& font, QWidget* pParent )
-//	: QWidget(pParent)
-//	, m_Text()
-//	, m_TextColor(Qt::color1)  // normal foreground
-//	, m_BGColor(Qt::transparent)  // normal background (transparent)
-//	, m_Alignment(Qt::AlignCenter)
-//	, m_pLayout(0)
-//	, m_timerId(-1)
-//	, m_timerDelay(0)
-//	, m_isDigitSize(false)
-//	, m_isVisible(true)
-//	, m_rotate(false)
-//{
-//	this->setFont( font );
-//	SetText( "ScaledText" );
-//}
-
-
-void ScaledText::SetText( const QString& text, bool rotate )
+void ScaledText::SetText( const QString& text,
+						  ETextSize size,
+						  bool rotate )
 {
-	m_Text = text;
+	set_size(size);
+
+	if( eSize_uppercase == m_textSize )
+		m_Text = text.toUpper();
+	else
+		m_Text = text;
+
 	m_rotate = rotate;
 
 	const QFont& currentFont = this->font();
 
 	delete m_pLayout;
-	//font.setPointSize(32);
-	m_pLayout = new QTextLayout(text, currentFont);
+	m_pLayout = new QTextLayout(m_Text, currentFont);
+	m_pLayout->setTextOption(QTextOption(m_Alignment));
 	m_pLayout->setCacheEnabled(true);
-//	QTextOption option(Qt::AlignLeft);
-//	option.setUseDesignMetrics(true);
-//	m_pLayout->setTextOption(option);
 	m_pLayout->beginLayout();
 	m_pLayout->createLine();
 	m_pLayout->endLayout();
@@ -59,8 +46,8 @@ void ScaledText::SetText( const QString& text, bool rotate )
 
 void ScaledText::SetFont( const QFont &font )
 {
-    this->setFont(font);
-    Redraw();
+	this->setFont(font);
+	Redraw();
 }
 
 void ScaledText::SetFontAndColor( const QFont& font, const QColor& textColor,
@@ -68,7 +55,7 @@ void ScaledText::SetFontAndColor( const QFont& font, const QColor& textColor,
 {
 	this->setFont(font);
 	SetColor( textColor, bgColor );
-    Redraw();
+	Redraw();
 }
 
 
@@ -97,14 +84,15 @@ const QColor& ScaledText::GetBgColor() const
 	return m_BGColor;
 }
 
-void ScaledText::SetDigitSize( bool digitSize )
+void ScaledText::set_size( ETextSize size )
 {
-	m_isDigitSize = digitSize;
+	Q_ASSERT(size < eSize_MAX && size >= 0);
+	m_textSize = size;
 }
 
-bool ScaledText::IsDigitSize() const
+ScaledText::ETextSize ScaledText::GetSize() const
 {
-	return m_isDigitSize;
+	return m_textSize;
 }
 
 void ScaledText::SetBlinking( bool blink, int delay )
@@ -130,45 +118,72 @@ bool ScaledText::IsBlinking() const
 
 void ScaledText::Redraw()
 {
-    SetText(m_Text);
+	update();
 }
 
 void ScaledText::paintEvent( QPaintEvent* event )
 {
 	QPainter painter(this);
 	painter.save();
+
+	// erase background
 	painter.fillRect( event->rect(), QBrush(m_BGColor) );
+
 	if( m_isVisible )
 	{
 		painter.setRenderHint( QPainter::TextAntialiasing );
-		if (m_pLayout != 0)
+		if (0 != m_pLayout)
 		{
 			painter.setPen( m_TextColor );
-			painter.translate(width() / 2, height() / 2);
+
+			// move coordinate system to the center
+			painter.translate(width() / 2.0, height() / 2.0);
 
 			QTextLine line = m_pLayout->lineAt(0);
 			line.setLeadingIncluded(false);
-			qreal w = m_rotate ?
-					  line.naturalTextRect().height() :
-					  line.naturalTextRect().width();
-			qreal h = m_rotate ?
-					  line.naturalTextRect().width() :
-					  line.naturalTextRect().height();
-			if( m_isDigitSize && !m_rotate )
-				h -= line.descent()*2;
-			qreal zoom_x = width()/w;
-			qreal zoom_y = height()/h;
-			zoom_x = std::min<qreal>(zoom_x,zoom_y);
-			zoom_y = zoom_x;
+
+			const QRectF rect = line.naturalTextRect();
+			Q_ASSERT( rect == m_pLayout->boundingRect() );
+
+			qreal w = m_rotate ? rect.height() : rect.width();
+			qreal h = m_rotate ? rect.width() : rect.height();
+
+			qreal adjust_y(0);
+			if( !m_rotate )
+			{
+				if( eSize_full == m_textSize )
+				{
+					h -= line.descent()*2 + 0.5;
+				}
+				else if ( eSize_uppercase == m_textSize )
+				{
+					h -= line.descent() + 2;
+					adjust_y = -line.descent()/2.5;
+				}
+				else
+				{
+					//adjust_y = line.descent()/2.0;
+				}
+			}
+
+			const qreal zoom = std::min<qreal>(width()/w, height()/h);
+
 			if( m_rotate )
 				painter.rotate(-60.0);
-			painter.scale(zoom_x, zoom_y);
 
-			QPointF center = line.naturalTextRect().center();
+			QPointF center = rect.center();
 			if( Qt::AlignLeft == m_Alignment )
-				center.setX(width()/2./zoom_x);
+			{
+				center.setX(width()/2.0/zoom);
+			}
 			else if( Qt::AlignRight == m_Alignment )
-				center.setX(-width()/2./zoom_x + center.x()*2);
+			{
+				center.setX(-width()/2.0/zoom + center.x()*2);
+			}
+
+			center.setY(center.y() + adjust_y);
+
+			painter.scale(zoom, zoom);
 			line.draw(&painter, -center);
 		}
 	}
--- widgets/scaledtext.h
+++ widgets/scaledtext.h
@@ -17,9 +17,17 @@ public:
 	ScaledText( QWidget* pParent = 0 );
 //	explicit ScaledText( const QFont& font, QWidget* pParent = 0 );
 
-	void SetText( const QString& text, bool rotate=false ); // has to be called after setting the font
-    void SetFont( const QFont& font );  // will call ReDraw() - default setFont() does not!
-    void SetFontAndColor( const QFont& font, const QColor& textColor,
+	enum ETextSize
+	{
+		eSize_normal = 0,
+		eSize_uppercase,
+		eSize_full,
+		eSize_MAX
+	};
+
+	void SetText( const QString& text, ETextSize size = eSize_normal, bool rotate = false ); // has to be called after setting the font
+	void SetFont( const QFont& font );  // will call ReDraw() - default setFont() does not!
+	void SetFontAndColor( const QFont& font, const QColor& textColor,
 						  const QColor& bgColor = Qt::transparent );
 	void SetColor( const QColor& textColor );
 	void SetColor( const QColor& textColor, const QColor& bgColor );
@@ -27,15 +35,13 @@ public:
 	const QColor& GetColor() const;
 	const QColor& GetBgColor() const;
 
-	void SetDigitSize( bool digitSize );
-	bool IsDigitSize() const;
+	ETextSize GetSize() const;
 
 	void SetBlinking( bool blink, int delay = 750 );
 	bool IsBlinking() const;
 
-    void Redraw();
+	void Redraw();
 
-	// overridden virtual functions
 	void setAlignment( Qt::AlignmentFlag flag )
 	{
 		m_Alignment = flag;
@@ -46,6 +52,8 @@ protected:
 	void timerEvent( QTimerEvent* event );
 
 private:
+	void set_size( ETextSize size );
+
 	QString m_Text;
 	QColor m_TextColor;
 	QColor m_BGColor;
@@ -53,7 +61,7 @@ private:
 	QTextLayout* m_pLayout;
 	int m_timerId;
 	int m_timerDelay;
-	bool m_isDigitSize;
+	ETextSize m_textSize;
 	bool m_isVisible;
 	bool m_rotate;
 };
