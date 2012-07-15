c4a1b38a87141114ca151d6fc174bde9aef27566
Florian Mücke <fmuecke@googlemail.com>
2012-07-15 12:19:01 +0200

    jom is now quiet (reduced build console output)

--- build.cmd
+++ build.cmd
@@ -15,21 +15,27 @@ SET BASE_DIR=%CD%
 SET BUILD_DIR=%BASE_DIR%\_build\build_output\~tmp
 SET BUILD_DIR_TEAM=%BASE_DIR%\_build\build_output\~tmp_TE
 
-IF "%VS100COMNTOOLS%"=="" (
-  CALL "%VS90COMNTOOLS%..\..\vc\vcvarsall.bat" x86
-  SET QMAKESPEC=win32-msvc2008
+IF NOT "%MINGW%"=="" (
+	SET QMAKESPEC=win32-g++
 ) ELSE (
-  CALL "%VS100COMNTOOLS%..\..\vc\vcvarsall.bat" x86
-  SET QMAKESPEC=win32-msvc2010
+	IF "%VS100COMNTOOLS%"=="" (
+	  CALL "%VS90COMNTOOLS%..\..\vc\vcvarsall.bat" x86
+	  SET QMAKESPEC=win32-msvc2008
+	) ELSE (
+	  CALL "%VS100COMNTOOLS%..\..\vc\vcvarsall.bat" x86
+	  SET QMAKESPEC=win32-msvc2010
+	)
 )
 
-IF NOT EXIST "%BOOST%\boost" (
+IF NOT EXIST "%BOOST_DIR%\boost" (
 	ECHO Can't find boost. Please set "BOOST" to boost path.
+	pause
 	GOTO :EOF
 )
 
 IF NOT EXIST "%QTDIR%\qmake.exe" (
 	ECHO Can't find qmake.exe. Please specify "QTDIR".
+	pause
 	GOTO :EOF
 )
 
@@ -52,6 +58,7 @@ if %errorlevel%==1 goto make_clean
 GOTO the_end
 
 :make_clean
+	echo;
 	echo --[make clean]--
 	rem del /Q "%BASE_DIR%\base\.buildnr"
 	rd /Q /S "%BASE_DIR%\bin"
@@ -59,11 +66,13 @@ GOTO the_end
 	qmake -recursive
 	if errorlevel 1 pause
 	jom /S /L clean>nul
+	jom /S /L clean>nul
 	if errorlevel 1 pause
 	if not "%1"=="internal" pause
 GOTO :EOF
 
 :build_clean
+	echo;
 	echo --[build clean]--
 	CALL :make_clean internal
 	CALL :build_incremental internal
@@ -71,15 +80,23 @@ GOTO :EOF
 GOTO :EOF
 
 :build_incremental
+	echo;
 	echo --[build incremental]--
 	qmake -recursive
 	if errorlevel 1 pause
-	jom /L /S release
-	if errorlevel 1 pause
+	::jom /L /S /F Makefile release
+	::if errorlevel 1 pause
+	CALL :do_compile core
+	CALL :do_compile gamepad
+	CALL :do_compile SingleTournament
+	CALL :do_compile TeamTournament
+	::CALL :do_compile VersionSelector
+	CALL :do_compile GamePadDemo
 	if not "%1"=="internal" pause
 GOTO :EOF
 
 :build_setup
+	echo;
 	echo --[setup only]--
 	if not exist "%INNO_DIR%\iscc.exe" (
 		echo Error: iscc.exe not found or INNO_DIR not defined!
@@ -98,11 +115,20 @@ GOTO :EOF
 GOTO :EOF
 
 :build_all
+	echo;
 	echo --[build all]--
 	CALL :make_clean internal
 	CALL :build_clean internal
 	CALL :build_setup internal
 	if not "%1"=="internal" pause
+GOTO :EOF
+
+:do_compile
+	echo -- compiling %1
+	pushd %1
+	jom /L /S /F Makefile.Release
+	if errorlevel 1 pause
+	popd
 GOTO :EOF
 
 :the_end
