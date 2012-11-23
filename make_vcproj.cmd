2e1ddda6517c56fa1ba424f93660daf2cdfb97ca
fmuecke <devnull@localhost>
2012-11-23 18:16:25 +0100

    fixed Visual Studio project creation script

--- make_vcproj.cmd
+++ make_vcproj.cmd
@@ -1,2 +1,28 @@
 @echo off
+if "%LIB%"=="" (
+	echo Visual Studio build environment not loaded
+	pause
+	goto :EOF
+)
+
+cd core
+qmake -tp vc
+cd ..
+
+cd gamepad
+qmake -tp vc
+cd ..
+
+cd gamepaddemo
+qmake -tp vc
+cd ..
+
+cd SingleTournament
+qmake -tp vc
+cd ..
+
+cd TeamTournament
+qmake -tp vc
+cd ..
+
 qmake -tp vc
