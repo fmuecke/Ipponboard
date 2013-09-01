6ec4f9bae57cdab01822d981aa93d0c8d0e3f3ae
Florian Mücke <fmuecke@googlemail.com>
2013-09-01 16:36:13 +0200

    added basic test for Score class

---
+++ test/tests.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "../core/score.h"
+
+using namespace std;
+using namespace Ipponboard;
+
+int main()
+{
+	try
+	{
+		Score::Test_Compare();
+		cout << "ok" << endl;
+	}
+	catch (int i)
+	{
+		cout << i << " failed" << endl;
+	}
+	
+	return 0;
+}
