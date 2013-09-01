64078432a8da78a8d2e6df2f320d5f6fad58c7a3
Florian Mücke <fmuecke@googlemail.com>
2013-09-01 22:10:36 +0200

    team: made 2013 rules the default for primer leagues

--- base/mainwindow.cpp
+++ base/mainwindow.cpp
@@ -1538,6 +1538,18 @@ void MainWindow::on_comboBox_mode_currentIndexChanged(const QString& s)
 {
     m_mode = s;
 
+	if (s == str_mode_1te_bundesliga_nord_f ||
+		s == str_mode_1te_bundesliga_sued_f ||
+		s == str_mode_1te_bundesliga_nord_m ||
+		s == str_mode_1te_bundesliga_sued_m)
+	{
+		m_pController->SetOption(eOption_Use2013Rules, true);
+	}
+	else
+	{
+		m_pController->SetOption(eOption_Use2013Rules, false);
+	}
+
     if( s == str_mode_1te_bundesliga_nord_m ||
         s == str_mode_1te_bundesliga_sued_m ||
         s == str_mode_2te_bundesliga_nord_m ||
