12e87ed7e8b7f7d22b57cf55216cad7f9976c56d
fmuecke <devnull@localhost>
2014-01-13 00:23:35 +0100

    buld.cmd uses local configuration file

--- build.cmd
+++ build.cmd
@@ -11,6 +11,22 @@
 ::---------------------------------------------------------
 @echo off
 SETLOCAL
+SET LOCAL_CONFIG=cfg.bat
+
+IF EXIST "%LOCAL_CONFIG%" (
+  CALL "%LOCAL_CONFIG%"
+  echo;
+) ELSE (
+  echo @echo off>"%LOCAL_CONFIG%"
+  echo set QTDIR=c:\development\qt\qt-4.8.5-vc12xp\bin>>"%LOCAL_CONFIG%"
+  echo set QMAKESPEC=win32-msvc2012>>"%LOCAL_CONFIG%"
+  echo set BOOST_DIR=c:\development\boost\boost_1_55_0>>"%LOCAL_CONFIG%"
+  ::echo set PATH=%QTDIR%;%PATH%>>%LOCAL_CONFIG%
+  echo Please configure paths in "%LOCAL_CONFIG%" first!
+  pause
+  GOTO :EOF
+)
+
 SET BASE_DIR=%CD%
 SET BUILD_DIR=%BASE_DIR%\_build\build_output\~tmp
 SET BUILD_DIR_TEAM=%BASE_DIR%\_build\build_output\~tmp_TE
@@ -40,9 +56,16 @@ IF NOT EXIST "%QTDIR%\qmake.exe" (
 )
 
 cls
-
+echo;
+echo Current config:
+echo;
+echo   QMAKESPEC : %QMAKESPEC%
+echo   QTDIR     : %QTDIR%
+echo   BOOST_DIR : %BOOST_DIR%
+echo;
 echo;
 echo Select build mode:
+echo;
 echo   (1) make clean
 echo   (2) clean build
 echo   (3) incremental build
@@ -65,7 +88,7 @@ GOTO the_end
 	rem del /Q "%BASE_DIR%\base\.buildnr"
 	rd /Q /S "%BASE_DIR%\bin"
 	rd /Q /S "%BASE_DIR%\lib"
-	qmake -recursive
+	"%QTDIR%"\qmake -recursive
 	if errorlevel 1 pause
 
 	jom /S /L clean>nul 
@@ -84,7 +107,7 @@ GOTO :EOF
 :build_incremental
 	echo;
 	echo --[build incremental]--
-	qmake -recursive
+	"%QTDIR%"\qmake -recursive
 	if errorlevel 1 pause
 	::jom /L /S /F Makefile release
 	::if errorlevel 1 pause
