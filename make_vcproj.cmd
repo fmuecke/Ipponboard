571cc86d0bca9b5ac9d22a83b906fdf5dc68db49
Florian Mücke <fmuecke@gmail.com>
2014-02-06 22:09:06 +0100

    updated makefile for vstudio

--- make_vcproj.cmd
+++ make_vcproj.cmd
@@ -1,32 +1,3 @@
 @echo off
-if "%LIB%"=="" (
-	echo Visual Studio build environment not loaded
-	pause
-	goto :EOF
-)
-
-cd core
-qmake -tp vc
-cd ..
-
-cd gamepad
-qmake -tp vc
-cd ..
-
-cd gamepaddemo
-qmake -tp vc
-cd ..
-
-cd SingleTournament
-qmake -tp vc
-cd ..
-
-cd TeamTournament
-qmake -tp vc
-cd ..
-
-cd Test
-qmake -tp vc
-cd ..
-
-qmake -tp vc
+call env_cfg.bat
+"%QTDIR%\qmake" -recursive -tp vc
