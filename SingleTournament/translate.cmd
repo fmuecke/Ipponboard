3be3f604313caa6897b524b627fc221f15ac53a4
Florian Muecke <fmuecke@gmail.com>
2014-03-25 23:17:12 +0100

    translate batch files now load the build environment

--- SingleTournament/translate.cmd
+++ SingleTournament/translate.cmd
@@ -1,6 +1,8 @@
 @echo off
-lupdate SingleTournament.pro
+call ..\env_cfg.bat
+
+"%QTDIR%\lupdate" SingleTournament.pro
 pause
-linguist ..\i18n\Ipponboard_de.ts
-lrelease -compress ..\i18n\Ipponboard_de.ts -qm ..\i18n\ipponboard_de.qm
+"%QTDIR%\linguist" ..\i18n\Ipponboard_de.ts
+"%QTDIR%\lrelease" -compress ..\i18n\Ipponboard_de.ts -qm ..\i18n\ipponboard_de.qm
 pause
--- core/translate.cmd
+++ core/translate.cmd
@@ -1,6 +1,7 @@
 @echo off
-lupdate core.pro
+call ..\env_cfg.bat
+"%QTDIR%\lupdate" core.pro
 pause
-linguist ..\i18n\core_de.ts
-lrelease -compress ..\i18n\core_de.ts -qm ..\i18n\core_de.qm
+"%QTDIR%\linguist" ..\i18n\core_de.ts
+"%QTDIR%\lrelease" -compress ..\i18n\core_de.ts -qm ..\i18n\core_de.qm
 pause
