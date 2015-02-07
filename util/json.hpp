38a33d590e8ed4969be209a5d2e6af267785bd48
Florian Mücke <fmuecke@gmail.com>
2015-02-07 04:30:02 +0100

    fulfilled json unit tests

--- util/json.hpp
+++ util/json.hpp
@@ -43,6 +43,28 @@ namespace fm
 			{}
 		};
 
+        static Json::Value ReadString(const std::string& str)
+        {
+            // skip byte-order-mark
+            auto content = str;
+            if (str[0] == (char)0xEF && str[1] == (char)0xBB && str[2] == (char)0xBF)
+            {
+                content = str.substr(3);
+            }
+
+            ::Json::Reader reader;
+            Json::Value json;
+
+            if (!reader.parse(content, json))
+            {
+                std::string message = "Invalid JSON:\n";
+                message.append(reader.getFormattedErrorMessages());
+                throw Exception(message);
+            }
+
+            return json;
+        }
+
 		static Json::Value ReadFile(const char * const & file)
 		{
 			if (!std::tr2::sys::exists(std::tr2::sys::path(file)))
@@ -54,9 +76,6 @@ namespace fm
 			std::stringstream buffer;
 			buffer << t.rdbuf();
 
-			::Json::Reader reader;
-			Json::Value json;
-
 			// skip byte-order-mark
 			auto content = buffer.str();
 			buffer.clear();
@@ -66,19 +85,21 @@ namespace fm
 				content = content.substr(3);
 			}
 
-			if (!reader.parse(content, json))
+			try
 			{
-				std::string message = "Error parsing JSON file '";
+				return ReadString(content);
+			}
+			catch (Exception const& e)
+			{
+				std::string message = "Error parsing file '";
 				message.append(file);
 				message.append("':\n");
-				message.append(reader.getFormattedErrorMessages());
+				message.append(e.what());
 				throw Exception(message);
 			}
-
-			return json;
 		}
 
-		static void WriteFile(const char * const & file, Json::Value const& json, bool writeBom = true)
+		static void WriteFile(const char * const & file, std::string const& jsonStr, bool writeBom = true)
 		{
 			try
 			{
@@ -90,7 +111,7 @@ namespace fm
 						t << (char)0xEF << (char)0xBB << (char)0xBF;
 					}
 
-					t << json.toStyledString();
+					t << jsonStr;
 					t.close();
 				}
 
@@ -99,6 +120,11 @@ namespace fm
 			{
 				throw FileAccessException(e.what());
 			}
+		}
+
+		static void WriteFile(const char * const & file, Json::Value const& json, bool writeBom = true)
+		{
+			return WriteFile(file, json.toStyledString(), writeBom);
 		}
 
 		static bool VerifyFile(const char * const & file, std::string& result)
