370f76a970b384f7641f0c82c643a11494a69ff5
Florian Mücke <fmuecke@gmail.com>
2015-05-24 10:20:50 +0200

    resized splash dialog

--- base/Main.cpp
+++ base/Main.cpp
@@ -192,16 +192,18 @@ int main(int argc, char* argv[])
                                           "it just takes a few seconds!");
     auto eulaText5 = QCoreApplication::tr("The most recent version can be found on the homepage:");
     auto eulaText6 = QCoreApplication::tr("Thank you very much!");
+	auto eulaText7 = QCoreApplication::tr("Thank you for using Ipponboard!");
 
     const QString text = QString(
                 "<html><body><h2><span style=\"color:#336699\">Ipponboard</span> <small>v%7</small></h2>"\
-                "<!--<p><strong>%1</strong></p>-->"\
+                "<p><em>%8</em></p>"\
+				"<p>%1</p>"\
                 "<p><span style=\"color:blue\"><em>%2</em></span></p>"\
                 "<p>%3</p>"\
                 "<p>%4</p>"\
                 "<p>%5 <a href=\"http://www.ipponboard.info\">www.ipponboard.info</a></p>"\
                 "<p><br/><em>%6</em></p>"\
-				"</body></html>").arg(eulaText1, eulaText2, eulaText3, eulaText4, eulaText5, eulaText6, VersionInfo::VersionStrShort);
+				"</body></html>").arg(eulaText1, eulaText2, eulaText3, eulaText4, eulaText5, eulaText6, VersionInfo::VersionStrShort, eulaText7);
 
 	SplashScreen::Data splashData;
 	splashData.text = text;
