dfdd46c5ab8743f7d6a7fa6cd06fd18b8f32bb34
Florian Mücke <fmuecke@gmail.com>
2015-07-20 07:19:17 +0200

    Added tests to ensure correct fight point calculation

--- test/TestFight.hpp
+++ test/TestFight.hpp
@@ -47,3 +47,48 @@ TEST_CASE("Fighter with less Shidos wins if points are equal")
 	REQUIRE_FALSE(fight.HasWon(second));
 	REQUIRE(fight.HasWon(first));
 }
+
+TEST_CASE("Validate score points (subscore)")
+{
+	auto first = FighterEnum::First;
+	auto second = FighterEnum::Second;
+
+	Fight fight;
+	fight.ruleSet = e2013RuleSet;
+
+	auto emptyScore = Score();
+	auto shidoScore = Score().Add(Point::Shido);
+	auto yukoScore = Score().Add(Point::Yuko);
+	auto yukoWithShidoScore = Score(yukoScore).Add(Point::Shido);
+	auto twoYukoScore = Score().Add(Point::Yuko).Add(Point::Yuko);
+	auto twoYukoWithShidoScore = Score(twoYukoScore).Add(Point::Shido);
+	auto twoYukoWithTwoShidoScore = Score(twoYukoScore).Add(Point::Shido).Add(Point::Shido);
+
+	auto IpponScore = Score().Add(Point::Ippon);
+
+	fight.scores[0] = emptyScore;
+	fight.scores[1] = shidoScore;
+	REQUIRE(fight.ScorePoints(first) == 1);
+	REQUIRE(fight.ScorePoints(second) == 0);
+
+	fight.scores[0] = yukoScore;
+	fight.scores[1] = yukoWithShidoScore;
+	REQUIRE(fight.ScorePoints(first) == 1);
+	REQUIRE(fight.ScorePoints(second) == 0);
+
+	// Hikewake
+	fight.scores[0] = twoYukoScore;
+	fight.scores[1] = twoYukoScore;
+	REQUIRE(fight.ScorePoints(first) == 0);
+	REQUIRE(fight.ScorePoints(second) == 0);
+
+	fight.scores[0] = yukoScore;
+	fight.scores[1] = twoYukoScore;
+	REQUIRE(fight.ScorePoints(first) == 0);
+	REQUIRE(fight.ScorePoints(second) == 5);
+
+	fight.scores[0] = twoYukoWithShidoScore;
+	fight.scores[1] = twoYukoWithTwoShidoScore;
+	REQUIRE(fight.ScorePoints(first) == 1);
+	REQUIRE(fight.ScorePoints(second) == 0);
+}
