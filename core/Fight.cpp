4e5803308540d2c8b5c9c4c69382e4b76e987485
Florian Mücke <fmuecke@gmail.com>
2015-07-20 22:52:44 +0200

    Revert "fight score will not result in yuko value (5) anymore if fight is won by shido (closes #40)"
    This reverts commit 62f0c8887cc252d83b68e4c768f52d335d409f6b.

--- core/Fight.cpp
+++ core/Fight.cpp
@@ -73,7 +73,7 @@ int Fight::ScorePoints(FighterEnum who) const
 			return eScore_Wazaari;
 		}
 
-		if (GetScore(who).Yuko() > GetScore(other).Yuko())
+        if (GetScore(who).Yuko() > 0)
 		{
             return eScore_Yuko;
 		}
