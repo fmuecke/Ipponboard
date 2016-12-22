b424f95327beae2b0ee32fd8a67f859e3797031d
Florian Mücke <fmuecke@gmail.com>
2016-12-22 20:14:42 +0100

    merged core translation files into base project

--- i18n/nl.ts
+++ i18n/nl.ts
@@ -1,6 +1,55 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="nl">
+<TS version="2.0" language="nl" sourcelanguage="en_US">
+<context>
+    <name>Ipponboard::Controller</name>
+    <message>
+        <location filename="../core/Controller.cpp" line="+386"/>
+        <source>First</source>
+        <translation>Eerste</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Second</source>
+        <translation>Tweede</translation>
+    </message>
+</context>
+<context>
+    <name>TournamentModel</name>
+    <message>
+        <location filename="../core/TournamentModel.cpp" line="+21"/>
+        <source>Weight</source>
+        <translation>Gewicht</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <location line="+8"/>
+        <source>Firstname Lastname</source>
+        <translation>Voornaam Achternaam</translation>
+    </message>
+    <message>
+        <location line="-2"/>
+        <location line="+8"/>
+        <source>Won</source>
+        <translation>Won</translation>
+    </message>
+    <message>
+        <location line="-7"/>
+        <location line="+8"/>
+        <source>Score</source>
+        <translation>Score</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Remaining</source>
+        <translation>Overblijvende</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Time</source>
+        <translation>Tijd</translation>
+    </message>
+</context>
 <context>
     <name>AddFighterDlg</name>
     <message>
