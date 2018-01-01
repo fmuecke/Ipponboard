dc8c4d7acaf8c5601563b9b2e36745763c6c1f36
Florian Mücke <fmuecke@gmail.com>
2018-01-01 08:45:24 +0100

    using external repo for documentation and 3rdParty licenses

--- doc/make_doc.cmd
+++
@@ -1,5 +0,0 @@
-pandoc -s ..\CHANGES.md -o ..\bin\changes.html --css=Ipponboard.css --self-contained
-pandoc -s ..\LICENSE.md -o ..\bin\license.html --css=Ipponboard.css --self-contained
-
-pandoc -s anleitung.md -o ..\bin\Anleitung.html --toc --css=Ipponboard.css --number-sections --self-contained
-
