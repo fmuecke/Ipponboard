003411da27459ded83c7b0be842e39049535f520
Ralf Bernhard <r_bernhard@gmx.de>
2024-02-06 18:40:59 +0100

    Linuxbuild scripts integrated

--- _build/linux/build_cmake_qt4-x64.cmd
+++ _build/linux/build_cmake_qt4-x64.cmd
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-#set -x
 
 function pause(){
    read -p "$*"
@@ -16,6 +15,7 @@ then
 else
   echo create file=$LOCAL_CONFIG
   echo QTDIR="/usr/lib64/qt4">>"$LOCAL_CONFIG"
+  source "$LOCAL_CONFIG"
 fi
 
 PWD=$(pwd)
@@ -25,12 +25,14 @@ echo BASE_DIR=$BASE_DIR
 source _create_versioninfo.linux $BASE_DIR/base
 
 #:: Create the QT resource file
-#rcc.exe -name ipponboard $BASE_DIR/base/ipponboard.qrc -o $BASE_DIR/base/qrc_ipponboard.cpp || exit 1
+rcc -name ipponboard $BASE_DIR/base/ipponboard.qrc -o $BASE_DIR/base/qrc_ipponboard.cpp 
 
 #:: now build
-#cmake -S "../_cmake_qt4" -B "../_build_cmake_qt4" -G "Visual Studio 17 2022" -A x64
-#cmake --build ../_build_cmake_qt4 --config Release --target Ipponboard
+cmake -S "../_cmake_qt4" -B "../_build_cmake_qt4"
+cmake --build ../_build_cmake_qt4 --config Release --target Ipponboard
+#cmake --build ../_build_cmake_qt4 --config Debug --target Ipponboard_Debug
 #cmake --build ../_build_cmake_qt4 --target setup 
 
+echo hit any key to finish
 pause
 
