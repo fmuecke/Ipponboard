bb894b04983e6593ebb97c64046ccb211bd40082
Ralf Bernhard <r_bernhard@gmx.de>
2024-02-24 23:15:00 +0100

    one build script is enough

--- _build/linux/build_cmake.cmd
+++ _build/linux/build_cmake.cmd
@@ -37,6 +37,132 @@ function create_env_qt5 {
   fi
 }
 
+#################
+function pause(){
+   read -p "$*"
+}
+
+######################
+function make_qt_res {
+  echo --[make_qt_res]--
+  source _create_versioninfo.linux $BASE_DIR/base || exit 1
+
+  # Create the QT resource file
+  rcc -name ipponboard $BASE_DIR/base/ipponboard.qrc -o $BASE_DIR/base/qrc_ipponboard.cpp || exit 1
+
+  echo make_qt_res finished, hit enter to continue
+}
+
+###########################
+function make_release {
+  echo --[make_release]--
+  CC=$(which gcc) CXX=$(which g++) cmake -S "../_cmake_$QT_VERSION" -B "../_build_cmake_$QT_VERSION" -DCMAKE_BUILD_TYPE=Release || exit 1
+
+  echo make_release finished, hit enter to continue
+}
+
+############################
+function build_release {
+  echo --[build_release]--
+  make_tests || exit 1
+
+  cmake --build ../_build_cmake_$QT_VERSION --target Ipponboard || exit 1
+  source _copy_files ../.. -release || exit 1
+  source _copy_files_$QT_VERSION ../.. -release || exit 1
+  BIN_DIR=$BASE_DIR/_build/bin/Release
+
+  echo build_release finished, hit enter to continue
+}
+
+################################
+function build_all_release {
+  echo --[build_all_release]--
+  clean
+  make_qt_res
+  make_release
+  build_release
+
+  echo build_all_release finished, hit enter to continue
+}
+
+#########################
+function make_debug {
+  echo --[make_debug]--
+  CC=$(which gcc) CXX=$(which g++) cmake -S "../_cmake_$QT_VERSION" -B "../_build_cmake_$QT_VERSION" -DCMAKE_BUILD_TYPE=Debug || exit 1
+
+  echo make_debug finished, hit enter to continue
+}
+
+
+##########################
+function build_debug {
+  echo --[build_debug]--
+  cmake --build ../_build_cmake_$QT_VERSION --target Ipponboard_Debug || exit 1
+  source _copy_files ../.. -debug || exit 1
+  BIN_DIR=$BASE_DIR/_build/bin/Debug
+
+  echo build_debug finished, hit enter to continue
+}
+
+####################
+function clean {
+  echo --[make_clean]--
+  if [ ! -z "$BIN_DIR" ]
+  then
+    rm -rf ../_build_cmake_$QT_VERSION
+    rm -rf $BIN_DIR
+  fi
+
+  echo clean finished, hit enter to continue
+}
+
+##############
+function run {
+  echo --[run]--
+  if [ ! -z "$BIN_DIR" ]
+  then
+    echo switching to directory [$BIN_DIR]...
+    pushd "$BIN_DIR"
+    LD_LIBRARY_PATH=. ./Ipponboard
+    popd
+  fi
+
+  echo run finished, hit enter to continue
+}
+
+#########################
+function make_tests {
+  echo --[make_tests]--
+  cmake --build ../_build_cmake_$QT_VERSION --target IpponboardTest || exit 1
+
+  pushd ../_build_cmake_$QT_VERSION/test
+  if ./IpponboardTest
+  then
+    popd
+    echo make_tests sucessfully finished
+  else
+    popd
+    echo make_tests with errors, exiting build
+    exit 1
+  fi
+}
+
+####################
+function translate {
+  echo --[make_translation]--
+  pushd $BASE_DIR
+  lupdate base core Widgets -locations relative -no-recursive -ts i18n/de.ts -ts i18n/nl.ts
+  linguist i18n/de.ts i18n/nl.ts
+  lrelease -compress i18n/de.ts -qm i18n/de.qm
+  lrelease -compress i18n/nl.ts -qm i18n/nl.qm
+  popd
+}
+
+###############
+function quit {
+  exit 0
+}
+
 ###############
 function menu {
   echo "Select the operation"
@@ -102,7 +228,6 @@ if [[ -z $ARCH ]] || [[ -z $QT_VERSION ]]; then
 fi
 
 create_env_$QT_VERSION
-source _common
 
 BIN_DIR=$BASE_DIR/_build/bin
 
