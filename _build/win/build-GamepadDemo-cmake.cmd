ffd394152aeaa6327ffc81740ed95e852ea219d0
Florian Mücke <fmuecke@gmail.com>
2024-03-18 17:41:19 +0100

    initial cmake build for ipponboard (todo: file copying)

---
+++ _build/win/build-GamepadDemo-cmake.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+call %~dp0init_env_cfg.cmd  || exit /b %errorlevel%
+
+set ROOT_DIR=%~dp0..\..
+
+:: Create makefiles
+cmake -S "%ROOT_DIR%\GamepadDemo" -B "%ROOT_DIR%\_build\build-GamepadDemo" -G "Visual Studio 17 2022" -A Win32 || exit /b %errorlevel%
+
+:: Build Release
+cmake --build "%ROOT_DIR%\_build\build-GamepadDemo" --clean-first --config Release
+
+pause
