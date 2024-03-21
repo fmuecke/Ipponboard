494f96a1677497cdfe96626da16ac57da7829b70
Florian Mücke <fmuecke@gmail.com>
2024-03-21 21:07:25 +0100

    working cmake build for Ipponboard (release,debug,clean) with file copy

---
+++ base/pch.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024 Florian Muecke. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE.txt file.
+
+#include "pch.h"
