65cf2674e587d07b70db7a1bc05b0dbd3a71ffaa
Florian Mücke <fmuecke@gmail.com>
2024-03-21 21:34:32 +0100

    fixed create-versioninfo calls

--- _build/win/build-Ipponboard-cmake.cmd
+++ _build/win/build-Ipponboard-cmake.cmd
@@ -7,9 +7,12 @@ if not "%1"=="" SET CONFIG=%1
 
 if "%2"=="clean" SET CLEAN=--clean-first
 
-:: Create makefiles
+:: create version information
+create-versioninfo.cmd %IPPONBOARD_ROOT_DIR%\base || exit /b %errorlevel%
+
+:: create makefiles
 cmake -S "%IPPONBOARD_ROOT_DIR%\base" -B "%IPPONBOARD_ROOT_DIR%\_build\build-Ipponboard" -G "Visual Studio 17 2022" -A Win32 || exit /b %errorlevel%
 
-:: Build Release
+:: build
 cmake --build "%IPPONBOARD_ROOT_DIR%\_build\build-Ipponboard" --config %CONFIG% %CLEAN%
 ::--clean-first
