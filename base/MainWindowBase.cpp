fmuecke/Ipponboard

commit d7cb083de17aa32ee56a894ad922541af1b25a02

fixed warnings: - FighterManagerDlg: local variable data hides member variable - MainWindowBase: desktop is depecated in Qt5; use screen - MainWindowTeam: desktop is depecated in Qt5; use screen; deprecated printer functions; local var data hides member - ModeManagerDlg: fixed returning reference - SettingsDlg: desktop is depecated in Qt5; use screen - View: variable redefinition - TournamentModel: return proper default flag value - json.hpp: using c++ cast

base/MainWindowBase.cpp
@@ -25,6 +25,9 @@
 #include <QUrl>
 #include <QTimer>
 #include <QStyle>
+#if QT_VERSION >= 0x050000
+#include <QScreen>
+#endif
 
 using namespace FMlib;
 using namespace Ipponboard;
@@ -60,8 +63,14 @@ void MainWindowBase::Init()
 	setWindowFlags(Qt::Window);
 	//setWindowState(Qt::WindowMaximized);
 	// instead, center window
-	this->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, this->size(),
-										  QApplication::desktop()->availableGeometry()));
+
+#if QT_VERSION >= 0x050000
+    auto rect = QGuiApplication::screens().first()->availableGeometry();
+#else
+    auto rect = QApplication::desktop()->availableGeometry();
+#endif
+
+    this->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, this->size(), rect));
 
 	load_fighters();
 
@@ -766,6 +775,34 @@ void MainWindowBase::on_actionPreferences_triggered()
 	}
 }
 
+void MainWindowBase::reset_second_screen_pos() const
+{
+#if QT_VERSION >= 0x050000
+    const auto nScreens = QGuiApplication::screens().size();
+    if (nScreens > 0 && nScreens > m_secondScreenNo)
+    {
+        auto screenRes = QGuiApplication::screens().at(m_secondScreenNo)->geometry();
+        m_pSecondaryView->move(QPoint(screenRes.x(), screenRes.y()));
+    }
+#else
+    const int nScreens(QApplication::desktop()->numScreens());
+    if (nScreens > 0 && nScreens > m_secondScreenNo)
+    {
+        auto screenRes = QApplication::desktop()->screenGeometry(m_secondScreenNo);
+        m_pSecondaryView->move(QPoint(screenRes.x(), screenRes.y()));
+    }
+#endif
+    if (m_secondScreenSize.isNull())
+    {
+        m_pSecondaryView->showFullScreen();
+    }
+    else
+    {
+        m_pSecondaryView->resize(m_secondScreenSize);
+        m_pSecondaryView->show();
+    }
+}
+
 void MainWindowBase::show_hide_view() const
 {
 	static bool isAlreadyCalled = false;
@@ -779,23 +816,7 @@ void MainWindowBase::show_hide_view() const
 
 	if (m_pSecondaryView->isHidden())
 	{
-		const int nScreens(QApplication::desktop()->numScreens());
-
-		if (nScreens > 0 && nScreens > m_secondScreenNo)
-		{
-			auto screenRes = QApplication::desktop()->screenGeometry(m_secondScreenNo);
-			m_pSecondaryView->move(QPoint(screenRes.x(), screenRes.y()));
-		}
-
-		if (m_secondScreenSize.isNull())
-		{
-			m_pSecondaryView->showFullScreen();
-		}
-		else
-		{
-			m_pSecondaryView->resize(m_secondScreenSize);
-			m_pSecondaryView->show();
-		}
+        reset_second_screen_pos();
 	}
 	else
 	{

base/MainWindowBase.h
@@ -121,6 +121,7 @@ protected:
 	void load_fighters();
 	void save_fighters();
 	virtual void update_views();
+    virtual void reset_second_screen_pos() const;
 	virtual void update_statebar();
 	virtual void update_info_text_color(const QColor& color, const QColor& bgColor);
 	virtual void update_text_color_first(const QColor& color, const QColor& bgColor);

base/MainWindowTeam.cpp
@@ -89,7 +89,7 @@ void MainWindowTeam::Init()
 
 	if (!Ipponboard::TournamentMode::ReadModes(MainWindowTeam::ModeConfigurationFileName(), modes, errMsg))
 	{
-		QMessageBox::critical(0,
+        QMessageBox::critical(nullptr,
 							  QCoreApplication::tr("Error reading mode configurations"),
 							  errMsg);
 
@@ -150,7 +150,7 @@ void MainWindowTeam::Init()
 	m_pUi->tableView_tournament_list2->setItemDelegateForColumn(TournamentModel::eCol_name2, cbxFightersGuest);
 #endif
 	// make name columns auto-resizable
-#if QT_VERSION_MAJOR >= 5
+#if QT_VERSION >= 0x050000
     m_pUi->tableView_tournament_list1->horizontalHeader()->setSectionResizeMode(TournamentModel::eCol_name1, QHeaderView::Stretch);
     m_pUi->tableView_tournament_list1->horizontalHeader()->setSectionResizeMode(TournamentModel::eCol_name2, QHeaderView::Stretch);
     m_pUi->tableView_tournament_list2->horizontalHeader()->setSectionResizeMode(TournamentModel::eCol_name1, QHeaderView::Stretch);
@@ -805,25 +805,7 @@ void MainWindowTeam::on_button_pause_clicked()
 	else
 	{
 		update_score_screen();
-		const int nScreens(QApplication::desktop()->numScreens());
-
-		if (nScreens > 0 && nScreens > m_secondScreenNo)
-		{
-			// move to second screen
-			QRect screenres =
-				QApplication::desktop()->screenGeometry(m_secondScreenNo);
-			m_pScoreScreen->move(QPoint(screenres.x(), screenres.y()));
-		}
-
-		if (m_secondScreenSize.isNull())
-		{
-			m_pScoreScreen->showFullScreen();
-		}
-		else
-		{
-			m_pScoreScreen->resize(m_secondScreenSize);
-			m_pScoreScreen->show();
-		}
+        reset_second_screen_pos();
 
 		m_pUi->button_pause->setText(tr("Hide results"));
 	}
@@ -995,9 +977,16 @@ void MainWindowTeam::on_actionPrint_triggered()
 	WriteScoreToHtml_();
 
 	QPrinter printer(QPrinter::HighResolution);
+#if QT_VERSION >= 0x050000
+    //TODO: fix margins (actual header margin is too big); preview crashed last time
+    printer.setPageOrientation(QPageLayout::Landscape);
+    printer.setPageSize(QPageSize(QPageSize::A4));
+    printer.setPageMargins(QMarginsF(15,10,15,5), QPageLayout::Millimeter);
+#else
 	printer.setOrientation(QPrinter::Landscape);
 	printer.setPaperSize(QPrinter::A4);
 	printer.setPageMargins(15, 10, 15, 5, QPrinter::Millimeter);
+#endif
 	QPrintPreviewDialog preview(&printer, this);
 	connect(&preview, SIGNAL(paintRequested(QPrinter*)), SLOT(Print(QPrinter*)));
 	preview.exec();
@@ -1036,11 +1025,20 @@ void MainWindowTeam::on_actionExport_triggered()
 		}
 		else
 		{
-			QPrinter printer(QPrinter::HighResolution);
-			printer.setOrientation(QPrinter::Landscape);
-			printer.setOutputFormat(QPrinter::PdfFormat);
-			printer.setPaperSize(QPrinter::A4);
-			printer.setPageMargins(15, 10, 15, 5, QPrinter::Millimeter);
+            QPrinter printer(QPrinter::HighResolution);
+#if QT_VERSION >= 0x050000
+            //TODO: fix margins (actual header margin is way to big)
+            //TODO: use QPdfWriter?
+            printer.setPageOrientation(QPageLayout::Landscape);
+            printer.setOutputFormat(QPrinter::PdfFormat);
+            printer.setPageSize(QPageSize(QPageSize::A4));
+            printer.setPageMargins(QMarginsF(15,10,15,5), QPageLayout::Millimeter);
+#else
+            printer.setOrientation(QPrinter::Landscape);
+            printer.setOutputFormat(QPrinter::PdfFormat);
+            printer.setPaperSize(QPrinter::A4);
+            printer.setPageMargins(15, 10, 15, 5, QPrinter::Millimeter);
+#endif
 			printer.setOutputFileName(fileName);
 			QTextEdit edit(m_htmlScore, this);
 			edit.document()->print(&printer);
@@ -1267,8 +1265,8 @@ void MainWindowTeam::copy_cell_content(QTableView* pTableView)
 
 	for (QModelIndex index : selection)
 	{
-		auto data = pTableView->model()->data(index, Qt::DisplayRole);
-		selectedText += data.toString() + '\n';
+        auto text = pTableView->model()->data(index, Qt::DisplayRole);
+        selectedText += text.toString() + '\n';
 	}
 
 	if (!selectedText.isEmpty())

base/CMakeLists.txt
@@ -178,7 +178,7 @@ add_executable(${CMAKE_PROJECT_NAME} WIN32 ${SOURCES} ${FORMS} ${OTHER})
 set_target_properties(${CMAKE_PROJECT_NAME} PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY_DEBUG ${OUTPUT_DIR_DEBUG}
     RUNTIME_OUTPUT_DIRECTORY_RELEASE ${OUTPUT_DIR}
-    COMPILE_FLAGS "/Zc:wchar_t- /MP"
+    COMPILE_FLAGS "/Zc:wchar_t- /MP /W4"
     CMAKE_SUPPRESS_REGENERATION true
 )
 

base/FighterManagerDlg.cpp
@@ -48,7 +48,7 @@ FighterManagerDlg::FighterManagerDlg(
 	ui->treeWidget_fighters->setColumnWidth(eColumn_weight, 50);
 	ui->treeWidget_fighters->setColumnWidth(eColumn_firstName, 100);
 	ui->treeWidget_fighters->setColumnWidth(eColumn_lastName, 100);
-#if QT_VERSION_MAJOR >= 5
+#if QT_VERSION >= 0x050000
     ui->treeWidget_fighters->header()->setSectionResizeMode(eColumn_firstName, QHeaderView::Stretch);
     ui->treeWidget_fighters->header()->setSectionResizeMode(eColumn_lastName, QHeaderView::Stretch);
 #else
@@ -407,7 +407,7 @@ void FighterManagerDlg::on_pushButton_settings_pressed()
 						"\nValid specifiers are: %1")
 					 .arg(Ipponboard::FighterManager::GetSpecifiererDescription());
 
-	QString data = QInputDialog::getText(this,
+    QString inputString = QInputDialog::getText(this,
 										 dlgTitle,
 										 dlgMsg,
 										 QLineEdit::Normal,
@@ -415,13 +415,13 @@ void FighterManagerDlg::on_pushButton_settings_pressed()
 										 &ok);
 
 	QString separator;
-	bool isValidSeparator = Ipponboard::FighterManager::DetermineSeparator(data, separator);
+    bool isValidSeparator = Ipponboard::FighterManager::DetermineSeparator(inputString, separator);
 
 	QStringList dataParts;
 
 	if (isValidSeparator)
 	{
-        dataParts = data.split(separator);
+        dataParts = inputString.split(separator);
 	}
 
 	// at least 3 parts must be set (first, last, ...)
@@ -431,23 +431,23 @@ void FighterManagerDlg::on_pushButton_settings_pressed()
 							  tr(""),
 							  tr("Invalid format. Please correct your input."));
 
-		data = QInputDialog::getText(this,
+        inputString = QInputDialog::getText(this,
 									 dlgTitle,
 									 dlgMsg,
 									 QLineEdit::Normal,
-									 data,
+                                     inputString,
 									 &ok);
 		isValidSeparator =
-			Ipponboard::FighterManager::DetermineSeparator(data, separator);
+            Ipponboard::FighterManager::DetermineSeparator(inputString, separator);
 
 		if (isValidSeparator)
 		{
-			dataParts = data.split(separator);
+            dataParts = inputString.split(separator);
 		}
 	}
 
 	if (ok && isValidSeparator)
 	{
-		m_formatStr = data;
+        m_formatStr = inputString;
 	}
 }

base/ModeManagerDlg.cpp
@@ -31,6 +31,8 @@ ModeManagerDlg::ModeManagerDlg(TournamentMode::List const& modes,
 	m_pUi->comboBox_rules->clear();
 	m_pUi->comboBox_rules->addItems(RulesFactory::GetNames());
 
+    m_DefaultMode = TournamentMode::Default();
+
 	if (!m_dialogData.empty())
 	{
 		int pos = 0;
@@ -161,7 +163,7 @@ void ModeManagerDlg::on_checkBox_allSubscoresCount_toggled(bool checked)
 
 void ModeManagerDlg::on_toolButton_add_clicked()
 {
-	TournamentMode& mode = TournamentMode::Default();
+    auto mode = m_DefaultMode;
 	mode.title = tr("*new*");
 	mode.listTemplate = m_pUi->comboBox_template->itemText(0);
 
@@ -312,5 +314,5 @@ Ipponboard::TournamentMode& ModeManagerDlg::GetMode(int i)
 		}
 	}
 
-	return std::move(TournamentMode());
+    return m_DefaultMode;
 }

base/ModeManagerDlg.h
@@ -56,6 +56,7 @@ private:
 	bool is_initialized() const { return m_currentIndex != -1;  }
 
 	Ipponboard::TournamentMode& GetMode(int i);
+    Ipponboard::TournamentMode m_DefaultMode{};
 	std::shared_ptr<Ui::ModeManagerDlg> m_pUi;  //TODO: use unique_ptr
 	int m_currentIndex;
 };

base/SettingsDlg.cpp
@@ -12,6 +12,9 @@
 #include <QDesktopWidget>
 #include <QFile>
 #include <QDir>
+#if QT_VERSION >= 0x050000
+#include <QScreen>
+#endif
 #include <QSound>
 #include <QColorDialog>
 
@@ -98,11 +101,19 @@ SettingsDlg::SettingsDlg(EditionType edition, QWidget* parent) :
 	ui->comboBox_hansokumake_second->addItems(buttons);
 
 	// num screens
-	int screens = QApplication::desktop()->numScreens();
+#if QT_VERSION >= 0x050000
+    int numScreens = QGuiApplication::screens().count();
+#else
+    int numScreens = QApplication::desktop()->numScreens();
+#endif
 
-	for (int i(1); i <= screens; ++i)
+    for (int i(1); i <= numScreens; ++i)
 	{
-		QRect res = QApplication::desktop()->screenGeometry(i - 1);
+#if QT_VERSION >= 0x050000
+        QRect res = QGuiApplication::screens().at(i - 1)->availableGeometry();
+#else
+        QRect res = QApplication::desktop()->screenGeometry(i - 1);
+#endif
 
 		ui->comboBox_screen->addItem(
 			QString("%1 (%2x%3)").arg(

commit e60a7ae70315cf9fa2e56241cf1e4250f8524dcd

fixed bug introduced in d7cb083: displaying scorescreen didn't work anymore

base/MainWindowBase.cpp
@@ -775,37 +775,37 @@ void MainWindowBase::on_actionPreferences_triggered()
 	}
 }
 
-void MainWindowBase::reset_second_screen_pos() const
+void MainWindowBase::update_screen_visibility(QWidget* pView) const
 {
 #if QT_VERSION >= 0x050000
     const auto nScreens = QGuiApplication::screens().size();
     if (nScreens > 0 && nScreens > m_secondScreenNo)
     {
         auto screenRes = QGuiApplication::screens().at(m_secondScreenNo)->geometry();
-        m_pSecondaryView->move(QPoint(screenRes.x(), screenRes.y()));
+        pView->move(QPoint(screenRes.x(), screenRes.y()));
     }
 #else
     const int nScreens(QApplication::desktop()->numScreens());
     if (nScreens > 0 && nScreens > m_secondScreenNo)
     {
         auto screenRes = QApplication::desktop()->screenGeometry(m_secondScreenNo);
-        m_pSecondaryView->move(QPoint(screenRes.x(), screenRes.y()));
+		pView->move(QPoint(screenRes.x(), screenRes.y()));
     }
 #endif
     if (m_secondScreenSize.isNull())
     {
-        m_pSecondaryView->showFullScreen();
+		pView->showFullScreen();
     }
     else
     {
-        m_pSecondaryView->resize(m_secondScreenSize);
-        m_pSecondaryView->show();
+		pView->resize(m_secondScreenSize);
+		pView->show();
     }
 }
 
 void MainWindowBase::show_hide_view() const
 {
-	static bool isAlreadyCalled = false;
+	static bool isAlreadyCalled = false;  // this line will only be called once!
 
 	if (isAlreadyCalled)
 	{
@@ -816,7 +816,7 @@ void MainWindowBase::show_hide_view() const
 
 	if (m_pSecondaryView->isHidden())
 	{
-        reset_second_screen_pos();
+        update_screen_visibility(m_pSecondaryView.get());
 	}
 	else
 	{

base/MainWindowBase.h
@@ -121,7 +121,7 @@ protected:
 	void load_fighters();
 	void save_fighters();
 	virtual void update_views();
-    virtual void reset_second_screen_pos() const;
+    virtual void update_screen_visibility(QWidget* pView) const;
 	virtual void update_statebar();
 	virtual void update_info_text_color(const QColor& color, const QColor& bgColor);
 	virtual void update_text_color_first(const QColor& color, const QColor& bgColor);

base/MainWindowTeam.cpp
@@ -805,7 +805,7 @@ void MainWindowTeam::on_button_pause_clicked()
 	else
 	{
 		update_score_screen();
-        reset_second_screen_pos();
+		update_screen_visibility(m_pScoreScreen.get());
 
 		m_pUi->button_pause->setText(tr("Hide results"));
 	}