e9b8741e15151be02a99354e1d3a5fd128f01c55
Florian Mücke <fmuecke@gmail.com>
2024-04-15 09:39:02 +0200

    dependent files (runtimes & data) are copied via cmake

--- _build/win/init_env_cfg.cmd
+++ _build/win/init_env_cfg.cmd
@@ -11,10 +11,8 @@ if exist "%LOCAL_CONFIG%" (
   echo set "IPPONBOARD_ROOT_DIR=c:\dev\_cpp\Ipponboard" >> "%LOCAL_CONFIG%"
   echo set "QMAKESPEC=win32-msvc2015" >> "%LOCAL_CONFIG%"
   echo set "QT4DIR=c:\devtools\qt\qt-4.8.7-vc14" >> "%LOCAL_CONFIG%"
-  echo set "QT5DIR=c:\devtools\qt5\5.15.2\msvc2019" >> "%LOCAL_CONFIG%"  
+  echo set "QTDIR=c:\devtools\qt5\5.15.2\msvc2019" >> "%LOCAL_CONFIG%"  
   echo set "BOOST_DIR=c:\devtools\boost_1_81_0" >> "%LOCAL_CONFIG%"
-  echo set "MSVC_DIR=C:\Program Files\Microsoft Visual Studio\2022\Professional\VC" >> "%LOCAL_CONFIG%"
-  echo set "MSVC_CMD=%%MSVC_DIR%%\Auxiliary\Build\vcvars32.bat" >> "%LOCAL_CONFIG%"
   echo set "INNO_DIR=c:\devtools\inno setup 5" >> "%LOCAL_CONFIG%"
   echo Please configure dependency paths in "%LOCAL_CONFIG%" first!
   pause
