cce0efeba81d8b9464bb7419db3286c09e141cba
Florian Mücke <fmuecke@gmail.com>
2024-05-02 21:01:01 +0200

    removed some qt4 references (still TODO: handling in cmake)

--- _build/win/init_env_cfg.cmd
+++ _build/win/init_env_cfg.cmd
@@ -10,8 +10,7 @@ if exist "%LOCAL_CONFIG%" (
   echo :: Configure dependency paths below  >> "%LOCAL_CONFIG%"
   echo set "IPPONBOARD_ROOT_DIR=c:\dev\_cpp\Ipponboard" >> "%LOCAL_CONFIG%"
   echo set "QMAKESPEC=win32-msvc2015" >> "%LOCAL_CONFIG%"
-  echo set "QT4DIR=c:\devtools\qt\qt-4.8.7-vc14" >> "%LOCAL_CONFIG%"
-  echo set "QTDIR=c:\devtools\qt5\5.15.2\msvc2019" >> "%LOCAL_CONFIG%"  
+  echo set "QTDIR=c:\devtools\qt5\Qt5.15.13-x86-msvc2022" >> "%LOCAL_CONFIG%"  
   echo set "BOOST_DIR=c:\devtools\boost_1_81_0" >> "%LOCAL_CONFIG%"
   echo set "INNO_DIR=c:\Program Files (x86)\Inno Setup 6\" >> "%LOCAL_CONFIG%"
   echo Please configure dependency paths in "%LOCAL_CONFIG%" first!
